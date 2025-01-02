89b42908afbc27874bb5fbee3a2e2cffec459469
Anupam vats <vatsanupam321@gmail.com>
2025-01-02 20:30:51 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,53 @@
+
+
+public class Loops {
+    public static void main(String[] args) {
+        //  do-while loop
+        /*int n;
+         Scanner sc = new Scanner(System.in);
+         System.out.println("enter number");
+         n= sc.nextInt();
+         int a=1;
+         do { 
+             System.out.println(a);
+             a++;
+         } while (a<=n); */
+
+    //    For loop
+    /*Scanner sc = new Scanner(System.in);
+        int n;
+         System.out.println("enter number");
+         n= sc.nextInt();
+         for(int i=1;i<=n;i++){
+            System.out.println(2*i);      //even number
+         }*/
+
+
+
+
+        //  Scanner sc = new Scanner(System.in);
+        // int n;
+        //  System.out.println("enter number");
+        //  n= sc.nextInt();
+        //  for(int i=1;i<=n;i++){
+        //     System.out.println(2*i+1);      //odd number
+        //  }
+
+
+        // decrementing loop
+
+        for(int i=9;i>0;i--){
+            System.out.println(i);
+        }
+
+
+
+
+
+
+
+
+
+    }
+    
+}
