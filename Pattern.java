596fc08b930c64ba987f558baceda9c412037f46
Anupam vats <vatsanupam321@gmail.com>
2025-01-02 21:27:24 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,112 @@
+public class Pattern {
+    public static void main(String[] args) {
+
+        // Inverted Half Pyramid
+        // for(int i=5;i>0;i--){
+        //     for(int j=0;j<i;j++){
+        //     System.out.print("*");
+        //     }
+        //     System.out.print("\n");
+
+        // }
+
+
+// *********************************************************
+
+
+        // Half Pyramid
+        // for(int i=0;i<5;i++){
+        //     for(int j=0;j<=i;j++){
+        //     System.out.print("*");
+        //     }
+        //     System.out.print("\n");
+
+        // }
+
+
+// ***********************************************************
+
+
+        // inverted half pyramid(rotated by 180 deg) 
+        // int n=4;
+        // for(int i=1;i<=n;i++){
+        //     // inner loop-> print space
+        //     for(int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     // inner loop-> print star
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+// **********************************************************
+
+        // Half Pyramid with Numbers
+        // int n=5;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(j+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+
+// ***********************************************************
+
+        // Inverted Half Pyramid with Number
+        // int n=5;
+        // for(int i=n;i>=1;i--){
+        //     for(int j=1;j<=i;j++){
+        //          System.out.print(j+" ");
+        //     }
+        //     System.out.print("\n");
+        // }
+
+        // 2nd Approach
+        // int n=5;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n-i+1;j++){
+        //         System.out.print(j+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+
+// ******************************************************
+
+
+        // solid rectangle
+
+        // for(int i=0;i<4;i++){
+        //     for(int j=0;j<5;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.print("\n");
+        // }
+
+       
+    //  **************************************************  
+       
+       
+        // Hollow rectangle
+
+        // int n=4,m=5;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        //         if(i==1||j==1||i==n||j==m){
+        //             System.out.print("*");
+        //         }else{
+        //             System.out.print(" ");
+        //         }            
+        //     }
+        //     System.out.println();
+
+        // }
+        
+    }
+}
